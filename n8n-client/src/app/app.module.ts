5a8aed9e7215e6aa128a86ced4ae4489c034b71a
Phu-not-Phu <danghomp@gmail.com>
2023-08-15 13:14:13 +0700

    Add Firebase Login

--- n8n-client/src/app/app.module.ts
+++ n8n-client/src/app/app.module.ts
@@ -5,6 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './@core/core.module';
 import { SharedModule } from './@shared/shared.module';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [
@@ -13,9 +17,11 @@ import { SharedModule } from './@shared/shared.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-
     CoreModule,
-    SharedModule
+    SharedModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ n8n-client/src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'n9n-project-8b290',
+    appId: '1:1027025749499:web:3f30ee0f5bbe4a7ba0775e',
+    storageBucket: 'n9n-project-8b290.appspot.com',
+    apiKey: 'AIzaSyBgiJXWPE81I1eeWWaaxolums1eoqwHhSQ',
+    authDomain: 'n9n-project-8b290.firebaseapp.com',
+    messagingSenderId: '1027025749499',
+    measurementId: 'G-TVJS9JMPC3',
+  },
+};
