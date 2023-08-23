4d619ec4c6efccbaebc49416ebb59a8ae95cec4a
KhanhNoober <khanh.nlq14917@sinhvien.hoasen.edu.vn>
2023-08-23 13:52:40 +0700

    in progress

--- n8n-server/src/types/nodes/nodes.controller.ts
+++ n8n-server/src/types/nodes/nodes.controller.ts
@@ -1,19 +1,30 @@
 import { Controller, Get, Query } from '@nestjs/common';
 import { NodesService } from './nodes.service';
+import { NodeGroup } from './models/node.model';
 
 @Controller('nodes')
 export class NodesController {
 
     constructor(private nodesService: NodesService) { }
 
-    @Get('')
-    async getNodes(@Query('name') name: string): Promise<any> {
+    @Get('name')
+    async getNodes(@Query('q') name: string): Promise<any> {
         return await this.nodesService.getNodes(name);
     }
 
     @Get('type')
-    async getNode(@Query('type') type: string): Promise<any> {
-        return await this.nodesService.getNode(type);
+    async getNode(@Query('q') type: string): Promise<any> {
+        return await this.nodesService.getNodeType(type);
+    }
+
+    @Get('properties')
+    async getNodeProperties(@Query('q') type: string): Promise<any> {
+        return await this.nodesService.getNodeProperties(type);
+    }
+
+    @Get('group')
+    async getNodesListByGroup(@Query('q') group: NodeGroup, @Query('only-name') onlyName: boolean): Promise<any> {
+        return await this.nodesService.getNodeListByGroup(group, onlyName);
     }
 
 }
