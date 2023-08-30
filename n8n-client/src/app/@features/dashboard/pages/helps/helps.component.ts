c667604d460693a986dfe84bfa6d0660408964f3
Phu-not-Phu <danghomp@gmail.com>
2023-08-30 15:22:40 +0700

    Finished Helps Page

--- n8n-client/src/app/@features/dashboard/pages/helps/helps.component.ts
+++ n8n-client/src/app/@features/dashboard/pages/helps/helps.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-helps',
   templateUrl: './helps.component.html',
-  styleUrls: ['./helps.component.scss']
+  styleUrls: ['./helps.component.scss'],
 })
 export class HelpsComponent {
+  docs_link = 'https://docs.n8n.io/';
+  course_link = 'https://www.youtube.com/watch?v=1MwSoB0gnM4';
 
+  openDocs() {
+    window.open(this.docs_link, '_blank');
+  }
+
+  openCourse() {
+    window.open(this.course_link, '_blank');
+  }
 }
