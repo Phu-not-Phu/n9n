29f3715bf008378bc7ee45097ef949e2f44efc81
Phu-not-Phu <danghomp@gmail.com>
2023-08-31 14:18:19 +0700

    Fixed Trang Credencials to Copy to Project

--- n8n-client/src/app/@shared/components/filter-box/credential/credential.component.ts
+++ n8n-client/src/app/@shared/components/filter-box/credential/credential.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'filter-credential',
   templateUrl: './credential.component.html',
-  styleUrls: ['./credential.component.scss']
+  styleUrls: ['./credential.component.scss'],
 })
 export class CredentialComponent {
+  _isOpen: boolean = false;
 
+  @Input() set isOpen(value: boolean) {
+    this._isOpen = value;
+  }
 }
