9e2cf880f089e394f85e4eee03ab93f54e19eb55
Phu-not-Phu <danghomp@gmail.com>
2023-09-06 19:51:59 +0700

    API Module Change

--- n8n-server/src/domains/api.module.ts
+++ n8n-server/src/domains/api.module.ts
@@ -3,9 +3,10 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { WorkflowModule } from './workflow/workflow.module';
 import { ProjectModule } from './project/project.module';
+import { NodesModule } from './types/nodes/nodes.module';
 
 @Module({
-  imports: [UsersModule, ProjectModule, WorkflowModule],
-  exports: [UsersModule, ProjectModule, WorkflowModule],
+  imports: [UsersModule, ProjectModule, WorkflowModule, NodesModule],
+  exports: [UsersModule, ProjectModule, WorkflowModule, NodesModule],
 })
 export class ApiModule {}
