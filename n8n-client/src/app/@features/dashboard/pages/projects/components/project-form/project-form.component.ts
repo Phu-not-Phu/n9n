9c6c546f4c4f5123d3b3e99553d7dcb20b4217d1
Phu-not-Phu <danghomp@gmail.com>
2023-09-06 15:33:38 +0700

    Alert Success Dialog

--- n8n-client/src/app/@features/dashboard/pages/projects/components/project-form/project-form.component.ts
+++ n8n-client/src/app/@features/dashboard/pages/projects/components/project-form/project-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Output, ViewChild } from '@angular/core';
 import {
   FormBuilder,
   FormControl,
@@ -7,6 +7,7 @@ import {
 } from '@angular/forms';
 import { Project } from '../../models/project.model';
 import { FormExport } from 'src/app/models/type-helper.model';
+import { DialogComponent } from 'src/app/@shared/components/dialog/dialog.component';
 
 @Component({
   selector: 'app-project-form',
@@ -17,9 +18,14 @@ export class ProjectFormComponent {
   constructor(private formBuilder: FormBuilder) {}
 
   _isSubmitted: boolean = false;
+  notificationMessage: string = 'Project created successfully!';
 
   @Output() submitForm = new EventEmitter<FormExport<Project>>();
   @Output() cancelForm = new EventEmitter<void>();
+
+  @ViewChild('notificationDialog') notificationDialog:
+    | DialogComponent
+    | undefined;
 
   projectFormGroups = this.formBuilder.group({
     name: new FormControl('', [Validators.required, Validators.minLength(6)]),
@@ -31,11 +37,12 @@ export class ProjectFormComponent {
 
     if (this.projectFormGroups.valid) {
       this.submitForm.emit(this.projectFormGroups.value);
-      this.cancelForm.emit();
+      this.notificationDialog?.openDialog();
     }
   }
 
   onCancel(): void {
     this.cancelForm.emit();
+    this.projectFormGroups.reset();
   }
 }
